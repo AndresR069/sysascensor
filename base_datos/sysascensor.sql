6291999bc571c8e3e440ce2aa77b343a3930c714
AndresR069 <a_ro69@hotmail.com>
2022-10-08 21:34:42 -0500

    cambio en la base de datos, descarguela de nuevo

--- base_datos/sysascensor.sql
+++ base_datos/sysascensor.sql
@@ -26,18 +26,21 @@ CREATE TABLE `ascensor` (
   `id_ascensor` int NOT NULL AUTO_INCREMENT,
   `nombre_lugar` varchar(90) DEFAULT NULL,
   `descripcion_ascensor` varchar(200) DEFAULT NULL,
-  `observacion` varchar(500) DEFAULT NULL,
+  `observacion` mediumblob,
   `id_persona` int DEFAULT NULL,
   `id_sector` int DEFAULT NULL,
   `id_asignacion` int DEFAULT NULL,
+  `id_bitacora` int DEFAULT NULL,
   PRIMARY KEY (`id_ascensor`),
   KEY `fk_id_persona_idx` (`id_persona`),
   KEY `fk_id_sector_idx` (`id_sector`),
   KEY `fk_id_asignacion_fallos_idx` (`id_asignacion`),
-  CONSTRAINT `fk_id_asignacion_fallos` FOREIGN KEY (`id_asignacion`) REFERENCES `asignacion_fallos` (`id_asignacion_fallos`),
-  CONSTRAINT `fk_id_persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
-  CONSTRAINT `fk_id_sector` FOREIGN KEY (`id_sector`) REFERENCES `sector` (`id_sector`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_id_bitacora_idx` (`id_bitacora`),
+  CONSTRAINT `fk_id_asignacion_fallos` FOREIGN KEY (`id_asignacion`) REFERENCES `asignacion_fallos` (`id_asignacion_fallos`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_bitacora` FOREIGN KEY (`id_bitacora`) REFERENCES `bitacora` (`id_bitacora`),
+  CONSTRAINT `fk_id_persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_sector` FOREIGN KEY (`id_sector`) REFERENCES `sector` (`id_sector`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +49,7 @@ CREATE TABLE `ascensor` (
 
 LOCK TABLES `ascensor` WRITE;
 /*!40000 ALTER TABLE `ascensor` DISABLE KEYS */;
-INSERT INTO `ascensor` VALUES (2,'dd','dd','dd',31,1,1);
+INSERT INTO `ascensor` VALUES (3,'fff','f',_binary 'v',30,1,49125,NULL),(9,'ll','ll',_binary 'll',30,1,49423,NULL),(11,'c','vv',_binary 'vvv',30,2,56238,NULL),(12,'','',_binary 'd',30,1,49404,NULL),(13,'perrq','bitch',_binary 'ffffdd',30,4,35241,NULL);
 /*!40000 ALTER TABLE `ascensor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,13 +62,13 @@ DROP TABLE IF EXISTS `asignacion_fallos`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `asignacion_fallos` (
   `id_asignacion_fallos` int NOT NULL,
-  `fecha_asignacion` date DEFAULT NULL,
+  `fecha_asignacion` varchar(10) DEFAULT NULL,
   `id_estado` int NOT NULL,
   PRIMARY KEY (`id_asignacion_fallos`,`id_estado`),
   UNIQUE KEY `id_asignacion_fallos_UNIQUE` (`id_asignacion_fallos`),
   KEY `id_asignacion_idx` (`id_asignacion_fallos`),
   KEY `fk_estado_idx` (`id_estado`),
-  CONSTRAINT `fk_id_estado` FOREIGN KEY (`id_estado`) REFERENCES `estado_fallos` (`id_estado_fallos`)
+  CONSTRAINT `fk_id_estado` FOREIGN KEY (`id_estado`) REFERENCES `estado_fallos` (`id_estado_fallos`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -75,8 +78,36 @@ CREATE TABLE `asignacion_fallos` (
 
 LOCK TABLES `asignacion_fallos` WRITE;
 /*!40000 ALTER TABLE `asignacion_fallos` DISABLE KEYS */;
-INSERT INTO `asignacion_fallos` VALUES (1,'2000-10-00',1);
+INSERT INTO `asignacion_fallos` VALUES (1,'2000-10-00',1),(35241,'2022-9-8',1),(35276,'2022-09-08',3),(49125,'2022-09-08',1),(49404,'2022-9-8',1),(49423,'2022-09-08',1),(56238,'2022-9-8',1),(60403,'2022-9-8',1);
 /*!40000 ALTER TABLE `asignacion_fallos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bitacora`
+--
+
+DROP TABLE IF EXISTS `bitacora`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bitacora` (
+  `id_bitacora` int NOT NULL AUTO_INCREMENT,
+  `fecha_bitacora` varchar(10) DEFAULT NULL,
+  `revision` varchar(999) DEFAULT NULL,
+  `postMantenimiento` varchar(999) DEFAULT NULL,
+  `id_situacion_mantenimiento` int DEFAULT NULL,
+  PRIMARY KEY (`id_bitacora`),
+  KEY `fk_situa_mantenimiento_idx` (`id_situacion_mantenimiento`),
+  CONSTRAINT `fk_situa_mantenimiento` FOREIGN KEY (`id_situacion_mantenimiento`) REFERENCES `situacion_mantenimiento` (`id_situacion_mantenimiento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bitacora`
+--
+
+LOCK TABLES `bitacora` WRITE;
+/*!40000 ALTER TABLE `bitacora` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bitacora` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -132,7 +163,7 @@ CREATE TABLE `persona` (
 
 LOCK TABLES `persona` WRITE;
 /*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `persona` VALUES (23,'Kevin David','Bautista','kevinrocha239@gmial.com','carrera','$2a$08$nCMftEMALFuADj0i8ZmBNucYn4OkoMHXbGeX8iKfl8g6y9lNhbc0e',0,1,'(310) 818-0061'),(30,'juan','rodriguez','carm_0987@hotmail.com','calle 2','$2a$08$u1KT1KGJXYJtiryfkO0bJ.A2tGsE07eGkEo4UnfMwwfEDPWA6FKNG',0,2,'3184351669'),(31,'pepito','alvarez','carodriguezmartinez@ucundinamarca.edu.co','calle 2','$2a$08$W/06CdIro7xrXaNvxCiy7eXiRTCb6We95alsdafL1cDC4FZAZ9gFO',0,2,'3184351669');
+INSERT INTO `persona` VALUES (23,'Kevin David','Bautista','kevinrocha239@gmial.com','carrera','$2a$08$nCMftEMALFuADj0i8ZmBNucYn4OkoMHXbGeX8iKfl8g6y9lNhbc0e',0,1,'(310) 818-0061'),(30,'juan','rodriguez','carm_0987@hotmail.com','calle 2','$2a$08$u1KT1KGJXYJtiryfkO0bJ.A2tGsE07eGkEo4UnfMwwfEDPWA6FKNG',0,2,'3184351669');
 /*!40000 ALTER TABLE `persona` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,6 +214,30 @@ LOCK TABLES `sector` WRITE;
 INSERT INTO `sector` VALUES (1,'Norte'),(2,'Sur'),(3,'Este'),(4,'Oeste'),(5,'Centro');
 /*!40000 ALTER TABLE `sector` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `situacion_mantenimiento`
+--
+
+DROP TABLE IF EXISTS `situacion_mantenimiento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `situacion_mantenimiento` (
+  `id_situacion_mantenimiento` int NOT NULL AUTO_INCREMENT,
+  `situacion_mantenimiento` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_situacion_mantenimiento`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `situacion_mantenimiento`
+--
+
+LOCK TABLES `situacion_mantenimiento` WRITE;
+/*!40000 ALTER TABLE `situacion_mantenimiento` DISABLE KEYS */;
+INSERT INTO `situacion_mantenimiento` VALUES (1,'POSPONER MANTENIMIENTO'),(2,'NO SE PUEDE ARREGLAR'),(3,'ARREGLADO EXITOSAMENTE');
+/*!40000 ALTER TABLE `situacion_mantenimiento` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -193,4 +248,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-06 17:42:45
+-- Dump completed on 2022-10-08 21:33:52
--- controllers/crud.js
+++ controllers/crud.js
@@ -1,51 +1,22 @@
 //Invocamos a la conexion de la DB
 const conexion = require('../database/db');
-//GUARDAR un REGISTRO Tecnico
-/*
-module.exports.guardar_tecnico = (req, res)=>{
-    const nombre_tecnico = req.body.nombre_tecnico;
-    const correo_tecnico = req.body.correo_tecnico;
-    const telefono = req.body.telefono;
-    conexion.query('INSERT INTO tecnico_encargado SET ?',{nombre_tecnico:nombre_tecnico, correo_tecnico:correo_tecnico, telefono:telefono}, (error, results)=>{
-        if(error){
-            console.log(error);
-        }else{
-           // console.log(results);   
-            res.redirect('/adminuser');         
-        }
-});
-};
-/*
-exports.saveTarea = (req, res)=>{
-    const falla = req.body.falla;
-    const correo_tecnico = req.body.correo_tecnico;
-    const telefono = req.body.telefono;
-    conexion.query('INSERT INTO asignacion_tarea SET ?',{nombre_tecnico:nombre_tecnico, correo_tecnico:correo_tecnico, telefono:telefono}, (error, results)=>{
-        if(error){
-            console.log(error);
-        }else{   
-            res.redirect('/tareas');         
-        }
-});
-};
-*/
 
 //Editar_Persona........
 exports.editar_persona = async (req, res) => {
   const id_persona = req.body.id_editar;
-  console.log(id_persona)
+ // console.log(id_persona)
   const nombres = req.body.nombres_editar;
   const apellidos = req.body.apellidos_editar;
   const email = req.body.email_editar;
   const direccion = req.body.direccion_editar;
-  console.log(direccion)
+ // console.log(direccion)
   const telefono = req.body.telefono_editar;
   const rol = req.body.rol_editar;
-  console.log(rol)
+ // console.log(rol)
   let id_rol = null;
   switch (rol) {//seccion de busqueda de id
     case "administrador":
-      console.log('entro a case administrador')
+      //console.log('entro a case administrador')
       id_rol = '1';
       break;
     case "tecnico":
@@ -106,27 +77,19 @@ exports.addTarea = async (req, res) => {
    // console.log('id del tecnico: ' + id_tecnico);
 
     //-------------------------------------------------------
-
-
-
     const observaciones = req.body.observaciones;
     //console.log(observaciones);
     const sector = req.body.sector;
    // console.log(sector);
-
     //datos tabla asignacion fallos
     const now = new Date(); //Extraer fecha actual 
     year = now.getFullYear();
     month = now.getMonth();
     day = now.getDate();
-
     let fechaActual = year + '-' + month + '-' + day;
     //console.log(fechaActual)
-
     const estado = req.body.estado;
     //console.log(estado);
-
-
 
     //switch Estado -------------------------------------
 
@@ -177,16 +140,12 @@ exports.addTarea = async (req, res) => {
         id_sector = "4";
         break;
     }
-
     //id_aleatorio no repetitivo de asignacion fallos
 
     // RECORDATORIO: Realizar funcion para verificar que no se repita
     let aleatorio = Math.random() * (100000);
     const id_random = Math.floor(aleatorio);
-
     //console.log(id_random)
-
-
     conexion.query(
       "INSERT INTO asignacion_fallos SET ?",
       {
@@ -224,4 +183,81 @@ exports.addTarea = async (req, res) => {
 };
 
 
-//--------------------------------------------------------------------------------------------------
+//------------METODO DE INSERCCION DE BITACORAS---------------------------------------
+
+exports.addBitacora = async (req, res) => {
+  //Metodo REGISTAR BITACORA
+  //captura los datos desde el front
+  try {
+    //datos tabla bitacora
+    const mensaje = req.body.mensaje;
+   // console.log('la falla es: ' + mensaje);
+    const postMantenimiento = req.body.postMantenimiento;
+   // console.log('ubicacion conjunto: ' + postMantenimiento);
+
+    //Manipulacion de la cadena tecnico para obtener solo id
+    const situacion = req.body.situacion;
+    //console.log('ubicacion conjunto: ' + situacion);
+
+    const now = new Date(); //Extraer fecha actual 
+    year = now.getFullYear();
+    month = now.getMonth();
+    day = now.getDate();
+    let fechaActual = year + '-' + month + '-' + day;
+    //console.log(fechaActual)
+    
+    //switch Estado -------------------------------------
+
+    let id_situacion = null;
+
+    switch (
+      situacion //seccion de busqueda de id
+    ) {
+      case "POSPONER MANTENIMINETO":
+       // console.log('entro a case estado en espera')
+        id_estado = "1";
+        break;
+      case "NO SE PUEDE ARREGLAR":
+      //  console.log('entro a case en curso')
+        id_estado = "2";
+        break;
+      case "ARREGLADO EXITOSAMENTE":
+        //console.log('entro a case finalizado')
+        id_estado = "3";
+        break;
+    }
+    conexion.query(
+      "INSERT INTO asignacion_fallos SET ?",
+      {
+        id_asignacion_fallos: id_random,
+        fecha_asignacion: fechaActual,
+        id_estado: id_estado
+
+      }, (error, results) => {
+        if (error) throw error;
+
+        conexion.query(
+          "INSERT INTO ascensor SET ?",
+          {
+            nombre_lugar: conjunto,
+            descripcion_ascensor: falla,
+            observacion: observaciones,
+            id_persona: id_tecnico,
+            id_sector: id_sector,
+            id_asignacion: id_random
+          },
+          (error, result) => {
+            if (error) throw error;
+
+            res.redirect("/tareas");
+
+          })
+      }
+    ); //al colocar query ya podemos especificar una sentencia MYSQL
+
+
+  } catch (error) {
+    console.log(error);
+  }
+
+};
