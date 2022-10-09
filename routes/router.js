7df827d5c8aed24c2e80d41d9aa3dd4f5837b3ae
AndresR069 <a_ro69@hotmail.com>
2022-10-08 20:55:08 -0500

    bitacora extrae datos

--- routes/router.js
+++ routes/router.js
@@ -27,21 +27,21 @@ router.get("/indexTec", (req, res) => {
     if (error) throw error;
     res.render("indexTec", {
       result: result,
-    
+
     });
   })
 
 
 });
 
-router.get('/hojamanten/:id', (req,res)=>{    
+router.get('/hojamanten/:id', (req, res) => {
   const id = req.params.id;
-  conexion.query('SELECT ascensor.id_ascensor, ascensor.nombre_lugar, ascensor.descripcion_ascensor, asignacion_fallos.fecha_asignacion, persona.direccion, sector.nombre_sector , persona.nombres, persona.apellidos, ascensor.observacion, estado_fallos.nombre_estado from ascensor INNER JOIN persona ON persona.id_persona = ascensor.id_persona INNER JOIN asignacion_fallos ON asignacion_fallos.id_asignacion_fallos = ascensor.id_asignacion INNER JOIN estado_fallos ON estado_fallos.id_estado_fallos = asignacion_fallos.id_estado inner join sector ON sector.id_sector = ascensor.id_sector WHERE  ascensor.id_ascensor=?',[id] , (error, results) => {
-      if (error) {
-          throw error;
-      }else{            
-          res.render('hojamanten', {tarea:results[0]});            
-      }        
+  conexion.query('SELECT ascensor.id_ascensor,ascensor.descripcion_ascensor, ascensor.nombre_lugar, ascensor.descripcion_ascensor, asignacion_fallos.fecha_asignacion, persona.direccion, sector.nombre_sector , persona.nombres, persona.apellidos, ascensor.observacion, estado_fallos.nombre_estado from ascensor INNER JOIN persona ON persona.id_persona = ascensor.id_persona INNER JOIN asignacion_fallos ON asignacion_fallos.id_asignacion_fallos = ascensor.id_asignacion INNER JOIN estado_fallos ON estado_fallos.id_estado_fallos = asignacion_fallos.id_estado inner join sector ON sector.id_sector = ascensor.id_sector WHERE  ascensor.id_ascensor=?', [id], (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.render('hojamanten', { tarea: results[0] });
+    }
   });
 });
 
@@ -52,36 +52,14 @@ router.get("/dashAdmiAs", (req, res) => {
 
 router.get("/index", (req, res) => {
   // anidacion de callbacks-- mas de una consulta mysql enviada a la misma vista
-  conexion.query('select ascensor.nombre_lugar,ascensor.descripcion_ascensor, ascensor.observacion,persona.direccion, persona.nombres, persona.apellidos from ascensor INNER JOIN persona ON persona.id_persona = ascensor.id_persona;', (error, result, fields) => {
-    if (error) throw error;
 
-    conexion.query('select count(*) as conteo from ascensor;', (error, resp, fields) => {
-      if (error) throw error;
-      res.render('index', {
-        result: result,
-        resp: resp
-      });
-    })
+  conexion.query('select count(*) as conteo from ascensor;', (error, resp, fields) => {
+    if (error) throw error;
+    res.render('index', {
+      resp: resp
+    });
   })
 });
-
-/*
-Codigo base para anidar llamadas a callback  ----------------------------------------
-router.post('/test', function (req, res, next) {
-db.query("select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'registros'; ", (error, results, fields) => {
-    if (error) throw error;
-    db.query("SELECT * FROM registros", (error, resp, fields) => {
-        if (error) throw error;
-        res.render('test', {
-            dataRegistros: resp,
-            columnNames: results
-        });
-    });
-});
-});
-*/
-// })
-
 
 router.get("/adminuser", (req, res) => { //Plantilla adminser -->tecnicos de usuario
   conexion.query('SELECT persona.id_persona, persona.nombres, persona.apellidos, persona.correo, persona.direccion,persona.telefono, persona.estado_logeo,rol.nombre_rol FROM persona inner join rol on persona.id_rol=rol.id_rol', (error, result) => {
@@ -109,35 +87,6 @@ router.get("/tareas", (req, res) => { //consulta de las tareas
 
   })
 });
-//.....METODOS del CRUD TECNICOS..............
-//Eliminar (recordatorio... pasarlo al archivo crud)
-/*
-router.get('/delete/:id_tecnico', (req, res) => {
-  const id_tecnico = req.params.id_tecnico;
-conexion.query('DELETE FROM tecnico_encargado WHERE id_tecnico = ?',[id_tecnico], (error, results)=>{
-    //conexion.query('DELETE FROM tecnico_encargado WHERE id_tecnico = ? INNER JOIN asignacion_tarea ON asignacion_tarea.id_tecnico = tecnico_encargado;',[id_tecnico], (error, results)=>{
-      if(error){
-          console.log(error);
-      }else{           
-          res.redirect('/adminuser');         
-      }
-  })
-});
-
-//.....METODOS del CRUD TAREAS..............
-/*
-router.get('/delete/:id_tarea', (req, res) => {
-  const id_tecnico = req.params.id_tarea;
- // console.log(id_tecnico)
-conexion.query('DELETE tecnico_encargado FROM tecnico_encargado INNER JOIN asignacion_tarea ON tecnico_encargado.id_tecnico = asignacion_tarea.id_tecnico WHERE asignacion_tarea.id_tecnico = ?',[id_tarea], (error, results)=>{
-      if(error){
-          console.log(error);
-      }else{           
-          res.redirect('/tareas');         
-      }
-  })
-});
-*/
 
 //router para los metodos del controller
 router.post('/register', authControllers.register)
